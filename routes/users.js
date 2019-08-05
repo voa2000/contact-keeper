bf1f565acff5ad69171a50cfd08d6e95085ced1f
voa2000 <voa2000@hotmail.com>
2019-08-05 23:01:11 +0100

    Added user data endpoint and test with postman

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
 
@@ -19,7 +20,12 @@ router.post(
     ).isLength({ min: 6 })
   ],
   (req, res) => {
-    res.send("Register a user");
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("Passed");
   }
 );
+
 module.exports = router;
