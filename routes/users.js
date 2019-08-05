a169c186e0c3ee943145cc8acca8f1b9384b0b48
voa2000 <voa2000@hotmail.com>
2019-08-05 22:44:08 +0100

    Moved users and rename

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const router = express.Router();
+
+const User = require("../models/User");
+
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post("/", (req, res) => {
-  res.send("Register a user");
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email address").isEmail(),
+    check(
+      "password",
+      "please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    res.send("Register a user");
+  }
+);
 module.exports = router;
